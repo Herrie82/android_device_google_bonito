0e57693ca2664cdd9d63025ad35e05853c95a463
Herrie <Github.com@herrie.org>
2024-02-28 05:58:39 +0100

    Update lineage_sargo.mk: Build only Halium bits

--- lineage_sargo.mk
+++ lineage_sargo.mk
@@ -2,8 +2,8 @@
 TARGET_SCREEN_HEIGHT := 2220
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Halium stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/bonito/aosp_sargo.mk)
